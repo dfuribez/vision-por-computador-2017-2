c87bd164c07c71b95a78b3a3bcbf7afe4d4f4e12
Diego Fernando <dfuribez@unal.edu.co>
2017-07-07 11:21:43 -0500

    Presentacion

--- parti.py
+++ parti.py
@@ -7,11 +7,14 @@ from core import morfo  # Aplicar operaciones morfológicas
 from core import common  # Operaciones comunes
 
 from matplotlib import pyplot as plt
+from scipy.misc import imsave
+
+plt.axis("off")
 
 IMAGEN = "img/partituras/burro_partitura1.jpg"
 #IMAGEN = "img/partituras/tetris.jpg"
 IMAGEN = "part.png"
-IMAGEN = "part3.png"
+IMAGEN = "part4.png"
 THRESH = 100
 KERNEL = np.array([[1, 1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1, 1],
@@ -75,6 +78,7 @@ def get_note(lineas, coors, prom):
         elif lineas[x] < fila_prom < lineas[x+1]:
             nota = NOMBRES_M[x]
         else:
+            pass
             #print(None)
         if nota is not None:
             return fila_prom, nota
@@ -97,8 +101,9 @@ if len(imagen.shape) == 3:
 print("invirtiendo xcolores...")
 imagen = np.amax(imagen) - imagen
 
-#plt.imshow(imagen, cmap="gray")
-#plt.show()
+plt.axis("off")
+plt.imshow(imagen, cmap="gray")
+plt.show()
 
 print("Aplicando umbral...")
 
@@ -106,24 +111,34 @@ print("Aplicando umbral...")
 imagen[imagen<THRESH] = 0
 imagen[imagen>=THRESH] = 1
 
-#plt.imshow(imagen, cmap="binary")
-#plt.show()
+plt.axis("off")
+plt.imshow(imagen, cmap="binary")
+plt.show()
 
 et, eti = etiquetar.etiquetar(imagen)
-
-#plt.imshow(et)
-#plt.show()
+imsave("etiquetada.png", et)
+plt.imshow(et)
+plt.show()
 
 
 lineas = mayor(et, eti, (20, 200))
 print(f"{len(lineas)} lineas encontradas")
 for line in lineas:
+    plt.axis("off")
+    plt.imshow(line, cmap="binary")
+    plt.show()
     linea_notas, coor_notas = find_lines(line)
+    plt.axis("off")
+    plt.imshow(linea_notas)
+    plt.show()
     prom =  dist_prom(coor_notas) // 3
     notas = morfo.core(line, KERNEL, "ero")
     notas[notas<10] = 0
     notas[notas>=10] = 1
     notas, _ = etiquetar.etiquetar(notas)
+    plt.axis("off")
+    plt.imshow(notas)
+    plt.show()
     filas, columnas = notas.shape
     recorridos = []
     b_notas = []
@@ -139,6 +154,7 @@ for line in lineas:
                         b_notas.append(note)
                     linea_notas[int(cx), columna] = 10
     print(", ".join(b_notas))
-    plt.imshow(line)
+    plt.axis("off")
+    plt.imshow(line, cmap="binary")
     plt.show()
 
